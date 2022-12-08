71bbd7f26d1520dcf9e6f59cd6c2d3a1dc70fd49
Timmy Yun <timur.unaspekov@nu.edu.kz>
2022-12-09 03:32:30 +0600

    Added models / db (postgre) connection TO DO: Integrate Celery to queue API requests

---
+++ main.py
@@ -0,0 +1,24 @@
+import requests
+import json
+
+
+URL = "http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=b763bfe1e59fe50ddb8c76103b96437a&artist=Cher&album=Believe&format=json"
+album_search_url = "http://ws.audioscrobbler.com/2.0/?method=album.search&album=nect&api_key=b763bfe1e59fe50ddb8c76103b96437a&format=json"
+artist_search = "http://ws.audioscrobbler.com/2.0/?method=artist.search"
+
+API_KEY = "b763bfe1e59fe50ddb8c76103b96437a"
+
+PARAM = {"api_key": API_KEY,
+         "format": "json",
+         "limit ": "1"}
+
+query = "Jo"
+
+PARAM["artist"] = query
+
+r = requests.get(url=artist_search, params=PARAM)
+
+data = r.json()
+
+with open("data.json", "w") as outfile:
+    json.dump(data, outfile)
--- timmyyun/api/models.py
+++ timmyyun/api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Queries(models.Model):
+    ''
+
+
+class Artist(models.Model):
+    name = models.CharField(max_length=30)
+    listeners = models.BigIntegerField()
+    mbid = models.CharField(max_length=36)
+    url = models.CharField(max_length=50)
+    streamable = models.CharField(max_length=25)
+    image_url = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = 'artist'
