f2faf1414f16fb638925c596443295002cd1265e
Timmy Yun <timur.unaspekov@nu.edu.kz>
2023-01-06 22:41:22 +0600

    deployment

--- timmyyun/settings.py
+++ timmyyun/settings.py
@@ -32,9 +32,9 @@ else:
     ALLOWED_HOSTS = []
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# if not IS_HEROKU:
-#     DEBUG = True
-DEBUG = False
+if not IS_HEROKU:
+    DEBUG = True
+
 
 # Application definition
 
@@ -176,7 +176,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     BASE_DIR / 'react-app/build/static'
