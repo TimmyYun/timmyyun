c94c4337fdc3676e0bc39af917088980146b5184
Timmy Yun <timur.unaspekov@nu.edu.kz>
2023-01-06 23:18:49 +0600

    deployment

--- timmyyun/settings.py
+++ timmyyun/settings.py
@@ -175,8 +175,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '/staticfiles')
+STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_DIRS = [
     BASE_DIR / 'react-app/build/static'
