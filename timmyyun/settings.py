05b367b9d7193eea4b48df170491cb05fe098f0f
Timmy Yun <timur.unaspekov@nu.edu.kz>
2023-01-07 00:31:04 +0600

    deployment

--- timmyyun/settings.py
+++ timmyyun/settings.py
@@ -175,11 +175,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'react-app/build/static'),
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'react-app/build/static')
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
