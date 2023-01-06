e9f8af1c7dfd24e2b6c9d05baad325c7a3d8c2b0
Timmy Yun <timur.unaspekov@nu.edu.kz>
2023-01-06 23:44:04 +0600

    deployment

--- timmyyun/settings.py
+++ timmyyun/settings.py
@@ -16,7 +16,7 @@ from django.test.runner import DiscoverRunner
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve()
 
 IS_HEROKU = "DYNO" in os.environ
 
